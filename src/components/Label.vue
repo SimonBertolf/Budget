144c40f39ff44102e020d85b2fdcc08b72366149
GojicDragan <gojic37@gmail.com>
2020-10-21 10:35:25 +0200

    vue, tailwind, storybook setup

---
+++ src/components/Label.vue
@@ -0,0 +1,21 @@
+<template>
+	<label class="text-xs text-blue-600" :for="id">
+		<slot></slot>
+	</label>
+</template>
+
+<script>
+  export default {
+    name: 'Label',
+		props: {
+      id: {
+        type: String,
+				required: true
+			}
+		}
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="relative before:border-none">
+		<select
+			:id="id"
+			class="appearance-none border border-gray-500 text-gray-600 w-full focus:outline-none pl-1 leading-tight text-sm font-hairline"
+		>
+			<option class="text-sm font-hairline" v-for="option in options" :key="option">
+				{{ option }}
+			</option>
+		</select>
+		<div class="box-border pointer-events-none absolute inset-y-0 right-0 flex items-center border border-gray-500">
+
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'Select',
+		props: {
+      id: {
+        type: String,
+				required: true
+			},
+      options: {
+        type: Array,
+				required: true
+			}
+		}
+  };
+</script>
+
+<style scoped>
+
+</style>
