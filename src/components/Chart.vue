9fa9a41b5b79e7b45d935313d142a742523889bd
Simon bertolf <simon.bertolf@reamis.ch>
2020-10-21 15:39:17 +0200

    Chart V1

---
+++ src/components/Chart.vue
@@ -0,0 +1,35 @@
+<template>
+ <Div></Div>
+</template>
+
+<script>
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+am4core.useTheme(am4themes_animated);
+
+export default {
+  name: 'Chart',
+  mounted() {
+    var chart = am4core.create('chartdiv', am4charts.PieChart);
+
+    Chart.data = [{
+      'country': 'Belgium',
+      'litres': 60,
+    }, {
+      'country': 'The Netherlands',
+      'litres': 50,
+    }];
+  },
+
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = 'litres';
+  pieSeries.dataFields.category = 'country';
+}
+</script>
+
+<style scoped>
+
+</style>
