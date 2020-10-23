944ca72b933126f05be2bf1992abe0384150b1ae
Simon bertolf <simon.bertolf@reamis.ch>
2020-10-23 19:04:24 +0200

    Button Hinzugefügt

---
+++ src/components/ButtonNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <Button :id="id" type="button" class="border border-black p-1"><slot></slot></Button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonNav',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/stories/ButtonNav.stories.js
@@ -0,0 +1,19 @@
+import { uuid } from 'vue-uuid';
+import ButtonNav from '../components/ButtonNav.vue';
+
+export default {
+  title: 'ButtonNav',
+  component: ButtonNav,
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { ButtonNav },
+  template: '<ButtonNav :id="id"> {{ text }} </ButtonNav>',
+});
+
+export const Example = Template.bind({});
+Example.args = {
+  id: uuid.v4(),
+  text: 'Test-Button',
+};
