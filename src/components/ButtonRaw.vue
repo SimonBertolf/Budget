f9a07c06485426481923b1479b60aea8b3b19450
Simon bertolf <simon.bertolf@reamis.ch>
2020-11-23 15:56:55 +0100

    Neues Backend Zeugs

---
+++ src/components/ButtonRaw.vue
@@ -0,0 +1,21 @@
+<template>
+	<Button :id="id" type="submit" class="bg-gray-300 p-1 rounded w-full ">
+		<slot></slot>
+	</Button>
+</template>
+
+<script>
+	export default {
+		name: 'ButtonRaw',
+		props: {
+			id: {
+				type: String,
+				required: true,
+			},
+		},
+	};
+</script>
+
+<style scoped>
+
+</style>
